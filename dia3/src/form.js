9b0646d1f4a409c7a9194b08ccd89d0baf850b51
Tiago Behenck <tiago.behenck@cwi.com.br>
2021-08-19 14:06:06 -0300

    :white_check_mark: solved exercise two day 3

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,4 +1,6 @@
 const inputName = document.querySelector('[data-js="name"]');
+const inputCores = document.querySelector('[data-js="cores"]');
+const divCores = document.querySelector('[data-js="mostrarCores"]');
 
 export const textCapitalize = (text) => (text && text.length
   ? text
@@ -16,4 +18,37 @@ export const textCapitalize = (text) => (text && text.length
 
 inputName.addEventListener('input', (e) => {
   inputName.value = textCapitalize(e.target.value)
+})
+
+const cores = {
+  vermelho: '#f00',
+  azul: '#0000ff',
+  amarelo: '#ffff00',
+  verde: '#008000',
+  preto: '#000',
+}
+
+function changeColor(colorParam) {
+  colorParam.map((option) => {
+    const divColorida = document.createElement('div');
+    const nomeCor = document.createElement('p');
+    if (option.selected) {
+      divColorida.style.width = '100px'
+      divColorida.style.height = '100px'
+      divColorida.style.margin = '24px'
+      divColorida.style.backgroundColor = cores[option.value]
+      nomeCor.style.color = '#fff'
+      nomeCor.textContent = option.value
+      divColorida.appendChild(nomeCor)
+      divCores.appendChild(divColorida);
+    }
+  })
+};
+
+inputCores.addEventListener('change', (e) => {
+  const selectedOpts = [...e.target.options].map(x => ({
+    value: x.value,
+    selected: x.selected,
+  }))
+  changeColor(selectedOpts)
 })
