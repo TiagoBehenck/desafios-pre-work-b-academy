19a148091882b8af9aaaebe94bb56b3365475f2e
Tiago Behenck <tiago.behenck@cwi.com.br>
2021-08-19 14:41:42 -0300

    :white_check_mark: solved exercise three day 3

---
+++ dia3/src/cars.js
@@ -0,0 +1,61 @@
+const formCar = document.querySelector('[data-js="form"]');
+const tableCar = document.querySelector('[data-js="table"]');
+
+const inserirNovaLinhaN = ({
+  imageUrl,
+  marca,
+  ano,
+  placa,
+  cor,
+}) => {
+  const newRow = tableCar.insertRow(-1)
+
+  const imageCell = newRow.insertCell(0)
+  const modeloCell = newRow.insertCell(1)
+  const anoCell = newRow.insertCell(2)
+  const placaCell = newRow.insertCell(3)
+  const corCell = newRow.insertCell(4)
+
+  const imgElement = document.createElement('img')
+  imgElement.src = imageUrl
+  imgElement.alt = marca
+  imageCell.appendChild(imgElement)
+
+  modeloCell.textContent = marca
+  anoCell.textContent = ano
+  placaCell.textContent = placa
+
+  const pintura = document.createElement('div')
+  pintura.style.width = '25px'
+  pintura.style.height = '25px'
+  pintura.style.backgroundColor = cor
+  corCell.appendChild(pintura)
+}
+
+formCar.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const imageUrl = formCar.imagem.value
+  const marca = formCar.marca.value
+  const ano = formCar.ano.value
+  const placa = formCar.placa.value
+  const cor = formCar.cor.value
+
+  const car = {
+    imageUrl,
+    marca,
+    ano,
+    placa,
+    cor,
+  }
+
+  inserirNovaLinhaN(car)
+
+  formCar.imagem.value = ''
+  formCar.marca.value = ''
+  formCar.ano.value = ''
+  formCar.placa.value = ''
+  formCar.cor.value = '#ffffff'
+
+  formCar.imagem.focus()
+})
