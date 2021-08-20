68c6bbc12ec9d8e8702ccc5f1c86eabf812f853a
Tiago Behenck <tiago.behenck@cwi.com.br>
2021-08-20 14:02:19 -0300

    :white_check_mark: solved exercise two day 4 and refactor

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,63 +1,83 @@
 import './style.css'
 
+const url = 'http://localhost:3333/cars'
+
 const formCar = document.querySelector('[data-js="form"]');
 const tableCar = document.querySelector('[data-js="table"]');
 
 const insertNewRow = ({
-  imageUrl,
-  marca,
-  ano,
-  placa,
-  cor,
+  image,
+  brandModel,
+  year,
+  plate,
+  color,
 }) => {
   const newRow = tableCar.insertRow(-1)
 
   const imageCell = newRow.insertCell(0)
-  const modeloCell = newRow.insertCell(1)
-  const anoCell = newRow.insertCell(2)
-  const placaCell = newRow.insertCell(3)
-  const corCell = newRow.insertCell(4)
+  const brandModelCell = newRow.insertCell(1)
+  const yearCell = newRow.insertCell(2)
+  const plateCell = newRow.insertCell(3)
+  const colorCell = newRow.insertCell(4)
 
   const imgElement = document.createElement('img')
-  imgElement.src = imageUrl
-  imgElement.alt = marca
+  imgElement.src = image
+  imgElement.alt = brandModel
   imageCell.appendChild(imgElement)
 
-  modeloCell.textContent = marca
-  anoCell.textContent = ano
-  placaCell.textContent = placa
+  brandModelCell.textContent = brandModel
+  yearCell.textContent = year
+  plateCell.textContent = plate
 
   const pintura = document.createElement('div')
   pintura.style.width = '25px'
   pintura.style.height = '25px'
-  pintura.style.backgroundColor = cor
-  corCell.appendChild(pintura)
+  pintura.style.backgroundColor = color
+  colorCell.appendChild(pintura)
 }
 
 formCar.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const imageUrl = formCar.imagem.value
-  const marca = formCar.marca.value
-  const ano = formCar.ano.value
-  const placa = formCar.placa.value
-  const cor = formCar.cor.value
+  const image = formCar.image.value
+  const brandModel = formCar.brandModel.value
+  const year = formCar.year.value
+  const plate = formCar.plate.value
+  const color = formCar.color.value
 
   const car = {
-    imageUrl,
-    marca,
-    ano,
-    placa,
-    cor,
+    image,
+    brandModel,
+    year,
+    plate,
+    color,
   }
+
+  console.log({ car })
 
   insertNewRow(car)
 
-  formCar.imagem.value = ''
-  formCar.marca.value = ''
-  formCar.ano.value = ''
-  formCar.placa.value = ''
-  formCar.cor.value = '#ffffff'
+  formCar.image.value = ''
+  formCar.brandModel.value = ''
+  formCar.year.value = ''
+  formCar.plate.value = ''
+  formCar.color.value = '#ffffff'
 
-  formCar.imagem.focus()
+  formCar.image.focus()
 })
+
+const fechDataCars = async () => {
+  const response = await fetch(url)
+
+  console.log(response)
+
+  if (!response.status === 200) {
+    return
+  }
+
+  const carsData = await response.json()
+
+  carsData.forEach(item => insertNewRow(item))
+}
+
+fechDataCars()
